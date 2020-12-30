c336ebd84825da73ebde0bde5c8f8408220cae38
luismescobarf <luismescobarf@gmail.com>
2020-12-30 16:25:15 -0500

    Verificador completo, pendiente de pruebas

--- verificadorSoluciones.py
+++ verificadorSoluciones.py
@@ -25,7 +25,7 @@ solucion = solucionVRP_Solver('./solutions/sol_csp50.txt')
 #Contenedor del reporte
 reporte = {}
 reporte['traslapes'] = { 'numero':0, 'tuplas':[] }
-reporte['itinerariosExtensos'] = { 'numero':0, 'itinerarios':[] }
+reporte['itinerariosExtensos'] = { 'numero':0, 'itinerarios':[], 'excesos':[] }
 reporte['costoInterno'] = 0
 reporte['costoCompleto'] = 0
 
@@ -35,24 +35,72 @@ for i in range(1,instancia['numBloques']+1):
     listadoBloques[i] = 0
 
 #Por cada uno de los itinerarios
-for itinerario in solucion[itinerarios]:
+indiceItinerario = 1
+for itinerario in solucion['itinerarios']:
     
     #Acumulador de la duración del itinerario
-    duracionItinerario = 0
+    duracionItinerario = 0   
     
     #Validar si se trata de un itinerario con bloque único
     if len(itinerario) == 1:
-        duracionItinerario += instancia['bloques'][ itinerario[0] -1 ]
         
+        #Incorporar el tiempo de servicio
+        duracionItinerario += instancia['bloques'][ itinerario[0] - 1 ]['duracion']
+        
+        #Checkear el bloque en el listado de control
+        listadoBloques[ itinerario[0] ] += 1       
+    
+    else:#Itinerarios con más de un bloque de trabajo o servicio
+    
+        #Por cada uno de los bloques del itinerario, revisar la transcición
+        for i in range(len(itinerario)-1):
+            
+            #Tiempo de servicio del itinerario actual
+            duracionItinerario += instancia['bloques'][ itinerario[i] - 1 ]['duracion']
+            
+            #Checkear los bloques en el listado de control
+            listadoBloques[ itinerario[i] ] += 1            
+            
+            #Incorporar el costo de cada transición
+            reporte['costoInterno'] += instancia['matrizTiemposTransicion'][ itinerario[i] , itinerario[i+1] ]
+            
+            #Revisar tiempo de transición (factibilidad)
+            if not(instancia['matrizTiemposTransicion'][ itinerario[i] , itinerario[i+1] ] == instancia['M']):
+                duracionItinerario += instancia['matrizTiemposTransicion'][ itinerario[i] , itinerario[i+1] ]
+            else:#Si es infactible, añadirlo al reporte              
+                
+                #Incrementar el contador de traslapes
+                reporte['traslapes']['numero'] += 1
+                
+                #Guardar la tupla con el id del itinerario y los bloques traslapados
+                reporte['traslapes']['tuplas'].append( (indiceItinerario, itinerario[i] , itinerario[i+1] ) )
+                
+                #Reflejar infactibilidad en la duración del itinerario
+                duracionItinerario = instancia['M']              
+        
+        
+        #Agregar tiempo del último bloque del itinerario        
+        duracionItinerario += instancia['bloques'][ itinerario[-1] - 1 ]['duracion']
+        
+        #Checkear el último del itinerario en la lista de control
+        listadoBloques[ itinerario[-1] ] += 1
+        
+        #Revisar duración del itinerario, incorporar el detalle de la infactibilidad si se presenta
+        if duracionItinerario > instancia['minutosJornada']:           
+            reporte['itinerariosExtensos']['numero'] += 1           
+            reporte['itinerariosExtensos']['itinerarios'].append(itinerario)
+            reporte['itinerariosExtensos']['excesos'].append(duracionItinerario - instancia['minutosJornada'])
+          
+        #Costo de salida para cada itinerario
+        reporte['costoCompleto'] += instancia['matrizCostos'][ 0 , itinerario[0] ]
+        
+        #Incrementar el índice de itinerarios para el reporte de errores
+        indiceItinerario += 1
     
     
-    #Por cada uno de los bloques del itinerario
-    for i in range(len(itinerario)-1):
-        
-        itinerario
-        
-        
-        listadoBloques[bloque] 
+#Mostrar reporte detallado
+pp.pprint(reporte)
+    
         
     
 
