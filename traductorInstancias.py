e65ec058f5f797ed9f85fb327b7af15d16dc1d5d
luismescobarf <luismescobarf@gmail.com>
2021-01-01 15:32:59 -0500

    Inserción del encabezado listo en el script de traducción de instancias

--- traductorInstancias.py
+++ traductorInstancias.py
@@ -19,30 +19,27 @@ solucion = solucionVRP_Solver('./solutions/vrpSolver_csp50.txt')#Solución sospe
 #solucion = solucionVRP_Solver('./solutions/sol_csp50_b.txt')#Solución VRPSolver tiempos de servicio corregidos, 27 itinerarios
 #solucion = solucionVRP_Solver('./solutions/sol_csp50_modeloPropuesto.txt')#Solución mesquita92+borcinova17, 27 itinerarios
 
-
-
 #Preparar encabezado de la instancia
+nombreInstancia = instancia['nombreInstancia'].split('/')[-1].split('.')[0]
+nombreInstanciaExtension = nombreInstancia + '_Solomon.txt'
+try:
+    
+    with open('./encabezado.txt') as encabezado, open('./beasleySolomonFormat/'+nombreInstanciaExtension, 'w') as nuevaInstanciaSolomon:
+        
+        #Agregar el nombre del caso de prueba en la primera línea
+        nuevaInstanciaSolomon.write(nombreInstancia)
+        
+        #Agregar todas las líneas del encabezado
+        for linea in encabezado:
+            nuevaInstanciaSolomon.write(linea)   
+        
+except:
+    print('Problemas abriendo el encabezado o el archivo de salida con la instancia en formato Solomon')
 
 
-#Ejemplo para transferencia de líneas
-# with open('path/to/input') as infile, open('path/to/output', 'w') as outfile:
-#     copy = False
-#     for line in infile:
-#         if line.strip() == "Start":
-#             outfile.write(line) # add this
-#             copy = True
-#         elif line.strip() == "End":
-#             outfile.write(line) # add this
-#             copy = False
-#         elif copy:
-#             outfile.write(line)
+#Insertar información de los nodos (bloques de servicio)
 
 
 
-# encabezado = "VEHICLE 
-# NUMBER     CAPACITY
-#   32         100000
-
-# CUSTOMER
-# CUST NO.  XCOORD.    YCOORD.    DEMAND   READY TIME   DUE DATE   SERVICE TIME"
+#Generar el archivo con los costos de los arcos (transiciones factibles)
 
