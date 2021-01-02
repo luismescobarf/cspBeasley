7fff28633a29e65a393b53024d4cb3113c64ee59
luismescobarf <luismescobarf@gmail.com>
2021-01-02 12:05:38 -0500

    Traductor funcionando correctamente para un input de una sola instancia Beasley

--- traductorInstancias.py
+++ traductorInstancias.py
@@ -19,12 +19,14 @@ solucion = solucionVRP_Solver('./solutions/vrpSolver_csp50.txt')#Solución sospe
 #solucion = solucionVRP_Solver('./solutions/sol_csp50_b.txt')#Solución VRPSolver tiempos de servicio corregidos, 27 itinerarios
 #solucion = solucionVRP_Solver('./solutions/sol_csp50_modeloPropuesto.txt')#Solución mesquita92+borcinova17, 27 itinerarios
 
+
 #Preparar encabezado de la instancia
 nombreInstancia = instancia['nombreInstancia'].split('/')[-1].split('.')[0]
 nombreInstanciaExtension = nombreInstancia + '_Solomon.txt'
+nombreInstanciaCostoArcosExtension = nombreInstancia + '_Solomon_costs.txt'
 try:
     
-    with open('./encabezado.txt') as encabezado, open('./beasleySolomonFormat/'+nombreInstanciaExtension, 'w') as nuevaInstanciaSolomon:
+    with open('./encabezado.txt') as encabezado, open('./beasleySolomonFormat/nodes/'+nombreInstanciaExtension, 'w') as nuevaInstanciaSolomon, open('./beasleySolomonFormat/arcs/'+nombreInstanciaCostoArcosExtension, 'w') as nuevaInstanciaSolomonArcos:
         
         #Agregar el nombre del caso de prueba en la primera línea
         nuevaInstanciaSolomon.write(nombreInstancia)
@@ -37,44 +39,74 @@ try:
         cuatroEspacios = '    '
         seisEspacios = '      '
         nueveEspacios = '         '
-        coordenadaVirtualBloque_XY = 1 
+        coordenadaVirtualBloque_XY = 0 
         
         #Insertar la información del depósito virtual
+        lineaDepositoVirtual = str()
+        #Número de cliente
+        lineaDepositoVirtual += cuatroEspacios + str(0) 
+        #Coordenada en x
+        lineaDepositoVirtual += seisEspacios + str(coordenadaVirtualBloque_XY)
+        #Coordenada en y
+        lineaDepositoVirtual += nueveEspacios + str(coordenadaVirtualBloque_XY)
+        #Demanda (nada en el depósito)
+        lineaDepositoVirtual += nueveEspacios + str(0) 
+        #Ready Time
+        lineaDepositoVirtual += nueveEspacios + str(0)
+        #Due Date
+        lineaDepositoVirtual += nueveEspacios + str(0)
+        #Service Time
+        lineaDepositoVirtual += nueveEspacios + str(0)
+        #Insertarla al archivo donde se está almacenando la instancia
+        nuevaInstanciaSolomon.write(lineaDepositoVirtual+'\n')
         
-        
+        #Mover coordenadas
+        coordenadaVirtualBloque_XY += 1
         
         #Realizar el proceso para los nodos (bloques de trabajo o servicios)
         for i in range(1,instancia['numBloques']+1):
             
             #Contenedor de la línea que se va a insertar
-            linea = str()
+            lineaNodo = str()
             
             #Número de cliente
-            linea += cuatroEspacios + str(i)
+            lineaNodo += cuatroEspacios + str(i)
             
             #Coordenada en x
-            linea += seisEspacios + str(coordenadaVirtualBloque_XY) 
+            lineaNodo += seisEspacios + str(coordenadaVirtualBloque_XY) 
             
             #Coordenada en y
-            linea += seisEspacios + str(coordenadaVirtualBloque_XY) 
+            lineaNodo += nueveEspacios + str(coordenadaVirtualBloque_XY) 
             
             #Demanda (valor unitario, virtual)
-            linea += seisEspacios + str(1) 
+            lineaNodo += nueveEspacios + str(1) 
             
             #Ready Time
-            
-            
+            lineaNodo += nueveEspacios + str( instancia['bloques'][i-1]['t0'] )         
+                        
             #Due Date
+            lineaNodo += nueveEspacios + str( instancia['bloques'][i-1]['tf'] )         
             
             #Service Time
+            lineaNodo += nueveEspacios + str( instancia['bloques'][i-1]['duracion'] )         
+            
+            #Insertarla al archivo donde se está almacenando la instancia
+            nuevaInstanciaSolomon.write(lineaNodo+'\n')
             
             #Mover coordenadas
-            coordenadaVirtualBloque_XY += 1
-            
-            
-            
+            coordenadaVirtualBloque_XY += 1           
                   
         #Generar el segundo archivo de la instancia, con los costos de los arcos (transiciones factibles)
+        for transicion in instancia['transiciones']:
+            
+            #Contenedor de las líneas con el detalle de las transiciones
+            lineaTransicion = str()
+            
+            #Incorporar los detalles provenientes del contenedor de la instancia
+            lineaTransicion += str(transicion['i']) + ' ' + str(transicion['j']) + ' ' + str(transicion['costo'])
+            
+            #Insertarla al archivo donde se está almacenando la instancia
+            nuevaInstanciaSolomonArcos.write(lineaTransicion+'\n')
         
 except:
     print('Problemas abriendo el encabezado o el archivo de salida con la instancia en formato Solomon')
