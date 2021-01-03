a4bb0fc4b9182f34a20d1e0479714e5f02c08dd1
luismescobarf <luismescobarf@gmail.com>
2021-01-03 17:38:58 -0500

    Ejecución por lote actualizada con la salida a archivos .dot y .pdf de la solución

--- CSP_VRPSolver/beasleyCSP.sh
+++ CSP_VRPSolver/beasleyCSP.sh
@@ -1,13 +1,22 @@
-./VRPSolver data/beasleySolomonFormat/nodes/csp50_Solomon.txt -o sol/csp50_Solomon.sol > out/csp50_Solomon.out
-#./VRPSolver data/beasleySolomonFormat/nodes/csp50_Solomon.txt -o sol/csp50_Solomon.sol -t sol/csp50_Solomon.tex > out/csp50_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp100_Solomon.txt -o sol/csp100_Solomon.sol > out/csp100_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp150_Solomon.txt -o sol/csp150_Solomon.sol > out/csp150_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp200_Solomon.txt -o sol/csp200_Solomon.sol > out/csp200_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp250_Solomon.txt -o sol/csp250_Solomon.sol > out/csp250_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp300_Solomon.txt -o sol/csp300_Solomon.sol > out/csp300_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp350_Solomon.txt -o sol/csp350_Solomon.sol > out/csp350_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp400_Solomon.txt -o sol/csp400_Solomon.sol > out/csp400_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp450_Solomon.txt -o sol/csp450_Solomon.sol > out/csp450_Solomon.out
-./VRPSolver data/beasleySolomonFormat/nodes/csp500_Solomon.txt -o sol/csp500_Solomon.sol > out/csp500_Solomon.out
-
+#Lote completo Beasley,96
+./VRPSolver data/beasleySolomonFormat/nodes/csp50_Solomon.txt -o sol/csp50_Solomon.sol -t sol/csp50_Solomon.dot > out/csp50_Solomon.out
+dot -Tpdf sol/csp50_Solomon.dot -o sol/csp50_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp100_Solomon.txt -o sol/csp100_Solomon.sol -t sol/csp100_Solomon.dot > out/csp100_Solomon.out 
+dot -Tpdf sol/csp100_Solomon.dot -o sol/csp100_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp150_Solomon.txt -o sol/csp150_Solomon.sol -t sol/csp150_Solomon.dot > out/csp150_Solomon.out 
+dot -Tpdf sol/csp150_Solomon.dot -o sol/csp150_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp200_Solomon.txt -o sol/csp200_Solomon.sol -t sol/csp200_Solomon.dot > out/csp200_Solomon.out 
+dot -Tpdf sol/csp200_Solomon.dot -o sol/csp200_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp250_Solomon.txt -o sol/csp250_Solomon.sol -t sol/csp250_Solomon.dot > out/csp250_Solomon.out 
+dot -Tpdf sol/csp250_Solomon.dot -o sol/csp250_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp300_Solomon.txt -o sol/csp300_Solomon.sol -t sol/csp300_Solomon.dot > out/csp300_Solomon.out 
+dot -Tpdf sol/csp300_Solomon.dot -o sol/csp300_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp350_Solomon.txt -o sol/csp350_Solomon.sol -t sol/csp350_Solomon.dot > out/csp350_Solomon.out 
+dot -Tpdf sol/csp350_Solomon.dot -o sol/csp350_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp400_Solomon.txt -o sol/csp400_Solomon.sol -t sol/csp400_Solomon.dot > out/csp400_Solomon.out 
+dot -Tpdf sol/csp400_Solomon.dot -o sol/csp400_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp450_Solomon.txt -o sol/csp450_Solomon.sol -t sol/csp450_Solomon.dot > out/csp450_Solomon.out 
+dot -Tpdf sol/csp450_Solomon.dot -o sol/csp450_Solomon.pdf
+./VRPSolver data/beasleySolomonFormat/nodes/csp500_Solomon.txt -o sol/csp500_Solomon.sol -t sol/csp500_Solomon.dot > out/csp500_Solomon.out 
+dot -Tpdf sol/csp500_Solomon.dot -o sol/csp500_Solomon.pdf
 
